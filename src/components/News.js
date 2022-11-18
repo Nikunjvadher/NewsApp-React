b297868a43d77f90d8a931fc7226d7604d48dfe3
Nikunj Vadher <nikunjjv27@gmail.com>
2022-11-18 14:58:06 +0530

    fetching and api from url and display news components

--- src/components/News.js
+++ src/components/News.js
@@ -2,54 +2,19 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export class News extends Component {
-
-    articles = [
-        {
-            "source": {
-                "id": null,
-                "name": "BuffaloBills.com"
-            },
-            "author": "Buffalo Bills",
-            "title": "Sunday's Bills-Browns game moved to Detroit, fans to receive ticket refunds purchased through the Bills or Ticketmaster - BuffaloBills.com",
-            "description": "The game will still be televised on CBS at 1 p.m.",
-            "url": "https://www.buffalobills.com/news/bills-browns-ford-field-detroit-fans-receive-ticket-refunds",
-            "urlToImage": "https://static.clubs.nfl.com/image/private/t_editorial_landscape_12_desktop/bills/ut0ygj0ifkwk6uwa0tia",
-            "publishedAt": "2022-11-18T04:23:54Z",
-            "content": "Bills EVP/COO Ron Raccuia and Bills GM Brandon Beane held a press conference via Zoom Thursday evening to discuss the change in location for Sunday's game and other important topics including ticket … [+2696 chars]"
-        },
-        {
-            "source": {
-                "id": null,
-                "name": "BBC News"
-            },
-            "author": "https://www.facebook.com/bbcnews",
-            "title": "North Korea fires ICBM into sea west of Japan - BBC",
-            "description": "It follows Thursday's warning of a \"fiercer\" response to US plans to boost its regional presence.",
-            "url": "https://www.bbc.com/news/world-asia-63672399",
-            "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/E4B3/production/_127674585_498a5b81b1572ad8960db0b331b5e2ab22e485a60_0_5078_33852000x1333.jpg",
-            "publishedAt": "2022-11-18T04:08:41Z",
-            "content": "North Korea has launched an intercontinental ballistic missile (ICBM), South Korea says.\r\nPyongyang's longer range missiles are designed to bring the US mainland within range.\r\nJapan's Coast Guard sa… [+2491 chars]"
-        },
-        {
-            "source": {
-                "id": "cnn",
-                "name": "CNN"
-            },
-            "author": "Dakin Andone, Amir Vera, Jamiel Lynch",
-            "title": "Federal appeals court grants Alabama death row inmate last-minute stay of execution - CNN",
-            "description": "The US Supreme Court has overruled a federal appeals court's stay of execution for Alabama death-row prisoner Kenneth Smith, allowing the state to proceed with Smith's execution before his death warrant expires at midnight Thursday.",
-            "url": "https://www.cnn.com/2022/11/17/us/kenneth-smith-alabama-execution/index.html",
-            "urlToImage": "https://media.cnn.com/api/v1/images/stellar/prod/221116085016-kenneth-smith-alabama-inmate.jpg?c=16x9&q=w_800,c_fill",
-            "publishedAt": "2022-11-18T03:48:00Z",
-            "content": "[Breaking news update, published at 11:44 p.m. ET]\r\nThe US Supreme Court has vacated a federal appeals courts stay of execution for Alabama death row prisoner Kenneth Smith, allowing the state to pro… [+6487 chars]"
-        }
-    ]
-
     constructor(){
         super();
         this.state = {
-           article : this.articles
+           article : []
         }
+    }
+
+    async componentDidMount(){
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=fc127c7be20d4f0b9e8acf1a9fbd0988"
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData);
+        this.setState({article: parsedData.articles})
     }
 
   render() {
@@ -58,16 +23,14 @@ export class News extends Component {
         <h4> News Monkey - Top Headlines </h4>
         <div className="row">
         {this.state.article.map((e) => {
-          return <div className="col-md-4 mb-3" key={e.url}>
+          return <div className="col-md-4 NewsCard mb-3" key={e.url}>
             <NewsItem
-              title={e.title.slice(0,45)}
-              description={e.description.slice(0,88)}
+              title={e.title?e.title.slice(0,45):""}
+              description={e.description?e.description.slice(0,88):""}
               imgUrl={e.urlToImage}
               newsUrl = {e.url}
             />
           </div>
-
-            // console.log(e);
         })}
         </div>
       </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,4 +1,6 @@
-import react , {Component} from 'react'
+import {Component} from 'react'
+// import defaultImg from '../../img/default-Img.png';
+import '../App.css'
 
 export class NewsItem extends Component {
     render(){
@@ -6,8 +8,8 @@ export class NewsItem extends Component {
         let {title , description , imgUrl , newsUrl} = this.props;
         return (
             <div className='my-3'>
-                <div className="card" style={{width: '18rem'}}>
-                    <img src={imgUrl} className="card-img-top" alt="..."/>
+                <div className="card" style={{width: '18rem' , height: '24rem'}}>
+                    <img src={!imgUrl?"https://media2.gmgroup.be/00_nm_logo.png":imgUrl} className={`card-img-top cardImg`} alt="..."/>
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
                         <p className="card-text">{description}...</p>
@@ -18,5 +20,6 @@ export class NewsItem extends Component {
         )
     }
 } 
+// defaultImg
 
 export default NewsItem;
